23c61be6a08ee4992ca8452b38a65a396d6c71b1
andres <delacraa@bc.edu>
2019-09-09 15:39:47 -0400

    2.10 complete

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var awesomeMessageLabel: UILabel!
- 
+    @IBOutlet weak var awesomeMessageLable: UILabel!
+    var index = 0
     
     
     override func viewDidLoad() {
@@ -20,17 +20,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You Are Awesome"
-        let message2 = "You Are Great"
-        let message3 = "You Are Amazing"
         
-        if awesomeMessageLabel.text == message1{
-            awesomeMessageLabel.text = message2
-        }else if awesomeMessageLabel.text == message2 {
-            awesomeMessageLabel.text = message3}
-        else {awesomeMessageLabel.text = message1 }
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Amazing!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You brighten my day!",
+                        "You make me smile!",
+                        "You are the bomb",
+                        "You Are Faboulous",
+                        "You are killing it",
+                        "You got this!"]
         
-    }
+        awesomeMessageLable.text = messages[index]
+        if index == messages.count - 1 {
+            index = 0 }
+            else {
+            index = index + 1}
+        
+//        let message1 = "You Are Awesome"
+//        let message2 = "You Are Great"
+//        let message3 = "You Are Amazing"
+//
+//        if awesomeMessageLabel.text == message1{
+//            awesomeMessageLabel.text = message2
+//        }else if awesomeMessageLabel.text == message2 {
+//            awesomeMessageLabel.text = message3}
+//        else {awesomeMessageLabel.text = message1 }
+//
+//    }
     
 }
-
